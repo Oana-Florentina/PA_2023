086b15f6c67ede409a43c636a165078444447e05
Oana-Florentina <121244115+Oana-Florentina@users.noreply.github.com>
2023-03-10 02:35:11 +0200

    Add files via upload

--- lab2_homework/CityLocation.java
+++ lab2_homework/CityLocation.java
@@ -1,16 +1,35 @@
+/**
 
+ CityLocation is a class that extends the Location class and represents a location in a city with a population.
+ */
 public class CityLocation extends Location {
+    /** The population of the city location. */
     private int population;
+    /**
 
+     Constructs a CityLocation object with the given parameters.
+     @param name the name of the city location
+     @param latitude the latitude of the city location
+     @param longitude the longitude of the city location
+     @param population the population of the city location
+     */
     public CityLocation(String name, double latitude, double longitude, int population) {
         super(name, latitude, longitude);
         this.population = population;
     }
+    /**
 
+     Returns the population of the city location.
+     @return the population of the city location
+     */
     public int getPopulation() {
         return population;
     }
+    /**
 
+     Returns the type of the location, which is "City" for CityLocation objects.
+     @return the type of the location, which is "City"
+     */
     @Override
     public String getType() {
         return "City";
