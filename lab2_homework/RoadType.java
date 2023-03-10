0c9e63b7e7ea370f9c1dc87ee6a2e852d6c2a3cf
Oana-Florentina <121244115+Oana-Florentina@users.noreply.github.com>
2023-03-10 03:05:26 +0200

    Add files via upload

--- lab2_homework/RoadType.java
+++ lab2_homework/RoadType.java
@@ -1,7 +1,19 @@
+
 /**
 
- RoadType is an enumeration class that represents a road.
+ An enumeration of different types of roads.
+ <p>Possible values include:</p>
+ <ul>
+ <li>Highways</li>
+ <li>Expressways</li>
+ <li>Country roads</li>
+ </ul>
  */
 public enum RoadType {
-    highways, express, country
+    highways,
+    express,
+    country;
+
+    private RoadType() {
+    }
 }
