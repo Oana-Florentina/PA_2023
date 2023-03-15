c885a5ee23bc412429f632557d5259ef5a00a523
Oana-Florentina <floryflorentina55@gmail.com>
2023-03-16 00:20:47 +0200

    idk

--- Lab_3/src/Homework/Lab3.java
+++ Lab_3/src/Homework/Lab3.java
@@ -15,16 +15,16 @@ public class Lab3 {
     public void homework() {
         Network network = new Network();
         Person name1 = new Person("Florentina", "13/03/2003");
-       Programmer name2 = new Programmer("Alex", "23/01/2002", "Java");
-        Designer  name3= new Designer("Bianca", "22/02/2001", "Photoshop");
+        Programmer name2 = new Programmer("Alex", "23/01/2002", "Java");
+        Designer name3 = new Designer("Bianca", "22/02/2001", "Photoshop");
         Company company1 = new Company("Amazon");
         List<Homework.Node> nodesList = new ArrayList<Node>();
 
-        name1.addRelationship(  name2, "Friends");
-        company1.addRelationship( name3, "boss");
-        company1.addRelationship( name2, "employee");
-        name1.addRelationship(  name3, "Friends");
-       network.addNode(name1);
+        name1.addRelationship(name2, "Friends");
+        company1.addRelationship(name3, "boss");
+        company1.addRelationship(name2, "employee");
+        name1.addRelationship(name3, "Friends");
+        network.addNode(name1);
         network.addNode(name2);
         network.addNode(name3);
         network.addNode(company1);
