963c97d9e7f955092eff299dbc51afe89ff1b93b
Oana-Florentina <121244115+Oana-Florentina@users.noreply.github.com>
2023-03-22 12:44:20 +0200

    Add files via upload

--- Lab4/src/Homework/Main.java
+++ Lab4/src/Homework/Main.java
@@ -1,38 +1,35 @@
 package homework;
-
+import com.github.javafaker.Faker;
 import java.util.*;
 import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-
-        var students = IntStream.rangeClosed(0, 3)
-                .mapToObj(i -> new Student("S" + i) )
-                .toArray(Student[]::new);
-        var projects = IntStream.rangeClosed(0, 3)
-                .mapToObj(i -> new Project("S" + i) )
-                .toArray(Project[]::new);
-
-        students[0].addAdmissibleProject(projects[0]);
-        students[0].addAdmissibleProject(projects[1]);
-        students[0].addAdmissibleProject(projects[2]);
-        students[1].addAdmissibleProject(projects[0]);
-        students[1].addAdmissibleProject(projects[1]);
-        students[2].addAdmissibleProject(projects[0]);
+        StudentProjectAllocationProblem problem = new StudentProjectAllocationProblem(5,8);
+        TreeSet<Project> projectSet = new TreeSet<>(Arrays.asList(problem.getProjects().toArray(new Project[0])));
 
 
-
-        LinkedList<Student> studentList = new LinkedList<>(Arrays.asList(students));
-
+        LinkedList<Student> studentList = new LinkedList<>(problem.getStudents());
         studentList.sort(Student::compareTo);
-
         System.out.println("Sorted Students:");
         studentList.forEach(student -> System.out.println(student.getName()));
 
-
-        TreeSet<Project> projectSet = new TreeSet<>(Arrays.asList(projects));
-
         System.out.println("Sorted Projects:");
         projectSet.forEach(project -> System.out.println(project.getName()));
+
+
+        List<Student> students = problem.getStudents();
+
+        students.get(0).addAdmissibleProject(projectSet.ceiling(new Project("Project 3")));
+        students.get(0).addAdmissibleProject(projectSet.ceiling(new Project("Project 4")));
+        students.get(2).addAdmissibleProject(projectSet.ceiling(new Project("Project 3")));
+        students.get(2).addAdmissibleProject(projectSet.ceiling(new Project("Project 4")));
+
+        List<Student> studentsWithFewerPreferences = problem.findStudentsWithFewerPreferences();
+        System.out.println("Students with fewer preferences than the average:");
+        studentsWithFewerPreferences.forEach(student -> System.out.println(student.getName()));
+
+        problem.getProjects();
+
     }
 }
