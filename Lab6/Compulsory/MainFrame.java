59571ff68e042c21d7a106d8eb27d53a91480b7a
Oana-Florentina <121244115+Oana-Florentina@users.noreply.github.com>
2023-04-06 23:06:55 +0300

    Update MainFrame.java

--- Lab6/Compulsory/MainFrame.java
+++ Lab6/Compulsory/MainFrame.java
@@ -5,18 +5,25 @@ import javax.swing.*;
 import java.awt.*;
 
 import static java.awt.BorderLayout.*;
-
+/**
+The MainFrame class that  is responsible for initializing the drawing application and its components.
+*/
 
 public class MainFrame extends JFrame {
     ConfigPanel configPanel;
     ControlPanel controlPanel;
     DrawingPanel drawingPanel;
-
+/**
+ * Constructor for the MainFrame class.
+ * Initializes the drawing application.
+ */
     public MainFrame() {
         super("My Drawing Application");
         init();
     }
-
+/**
+ * Initializes the drawing application components.
+ */
     private void init() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         
