6c9dee20b5e7361422a94d6742b0c54fc1339d33
Oana-Florentina <121244115+Oana-Florentina@users.noreply.github.com>
2023-04-06 23:08:14 +0300

    Update Main.java

--- Lab6/Compulsory/Main.java
+++ Lab6/Compulsory/Main.java
@@ -1,5 +1,7 @@
 package Compulsory;
-
+/**
+*It creates a new instance of the MainFrame.
+*/
 public class Main {
     public static void main(String[] args) {
         MainFrame mainframe = new MainFrame();
