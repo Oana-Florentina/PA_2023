94acb9de6db5f1e01375f466e7cf18329e257d8b
Oana-Florentina <121244115+Oana-Florentina@users.noreply.github.com>
2023-04-06 22:41:47 +0300

    Add files via upload

--- Lab6/Compulsory/ControlPanel.java
+++ Lab6/Compulsory/ControlPanel.java
@@ -1,8 +1,12 @@
 package Compulsory;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class ControlPanel extends JPanel {
     final MainFrame frame;
@@ -39,9 +43,25 @@ public class ControlPanel extends JPanel {
         add(resetButton);
         add(exitButton);
         exitButton.addActionListener(this::exitGame);
+        saveButton.addActionListener(this::saveImage);
     }
 
     private void exitGame(ActionEvent e) {
         frame.dispose();
+    }
+    private void saveImage(ActionEvent e) {
+        JFileChooser fileChooser = new JFileChooser();
+        int userSelection = fileChooser.showSaveDialog(frame);
+
+        if (userSelection == JFileChooser.APPROVE_OPTION) {
+            File fileToSave = fileChooser.getSelectedFile();
+            BufferedImage image = frame.drawingPanel.image;
+            try {
+                ImageIO.write(image, "PNG", fileToSave);
+                JOptionPane.showMessageDialog(frame, "Image saved successfully!");
+            } catch (IOException exception) {
+                JOptionPane.showMessageDialog(frame, "Error saving image: " + exception.getMessage());
+            }
+        }
     }
 }
