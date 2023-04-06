35864ae0311de0a791d06b354ed1dc07bb6f6548
Oana-Florentina <121244115+Oana-Florentina@users.noreply.github.com>
2023-04-06 23:05:11 +0300

    Update Line.java

--- Lab6/Compulsory/Line.java
+++ Lab6/Compulsory/Line.java
@@ -1,12 +1,19 @@
 package Homework;
 import java.awt.*;
+/**
 
+Class that represents a line connecting two dots.
+*/
 public class Line {
     private Dot dot1;
     private Dot dot2;
     private Color color;
 
-
+/**
+ * Constructs a line object that connects two dots.
+ * @param dot1 the first dot in the line
+ * @param dot2 the second dot in the line
+ */
     public Line(Dot dot1, Dot dot2) {
         this.dot1 = dot1;
         this.dot2 = dot2;
